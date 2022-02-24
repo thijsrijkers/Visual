24a7c8a4dd332c70d832039bcde449f62c072373
Thijs R <thijs@qict.nl>
2022-02-24 13:51:29 +0100

    +Added a target in the package.json & added a dist check so there is always a dist to push to

--- src/init.ts
+++ src/init.ts
@@ -1,12 +1,16 @@
 import {Configure} from './conf';
+const fs = require("fs");
 
 const conf = new Configure();
 cloneRepo();
 
 async function cloneRepo(): Promise<void> {
     try {
+        if (!fs.existsSync('dist')) 
+            fs.mkdirSync('dist')
+
         await conf.git.init();
-        await conf.git.clone('https://github.com/thijsrijkers/Enchantment.git', './dist')
+        await conf.git.clone(JSON.parse(fs.readFileSync('package.json', 'utf8'))['target'], './dist')
     } catch (error) {
         await conf.git.pull()
     }
